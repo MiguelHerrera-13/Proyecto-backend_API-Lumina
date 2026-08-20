d73293146b55a1b6d4ff74c4efbe564d530ae7b1
MiguelH <migueljara592@gmail.com>
2026-08-20 11:40:19 -0300

    Commit inicial: Estructura base de Spring Boot

---
+++ src/main/java/com/miguel/api_lumina/ApiLuminaApplication.java
@@ -0,0 +1,13 @@
+package com.miguel.api_lumina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLuminaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLuminaApplication.class, args);
+	}
+
+}
