65b0a4dfe52e39b62807e5766724313ef37c18e3
MiguelH <migueljara592@gmail.com>
2026-09-16 10:48:14 -0300

    fix: Eliminar import sin uso en UsuarioControllerTest

--- src/test/java/com/miguel/api_lumina/controller/UsuarioControllerTest.java
+++ src/test/java/com/miguel/api_lumina/controller/UsuarioControllerTest.java
@@ -6,7 +6,6 @@ import com.miguel.api_lumina.dto.UsuarioResponseDTO;
 import com.miguel.api_lumina.exception.EmailYaRegistradoException;
 import com.miguel.api_lumina.service.UsuarioService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
